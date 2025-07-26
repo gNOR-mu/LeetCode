b61f4fb10c69601a485689e1a9f8533f4e29eaf3
gNOR-mu <82237339+gNOR-mu@users.noreply.github.com>
2025-07-26 01:13:50 -0400

    Time: 7 ms (14.78%) | Memory: 44.1 MB (60.59%) - LeetSync

--- 9-palindrome-number/palindrome-number.java
+++ 9-palindrome-number/palindrome-number.java
@@ -1,5 +1,6 @@
 class Solution {
     public boolean isPalindrome(int x) {
+        if(x<0){return false;}
         StringBuilder sb = new StringBuilder();
         sb.append(x);
         return sb.toString().equals(sb.reverse().toString());
