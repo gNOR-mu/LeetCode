2086c071d12af81ad45150ef4a90d8eb0cf73f98
gNOR-mu <82237339+gNOR-mu@users.noreply.github.com>
2025-07-26 00:58:45 -0400

    Time: 2 ms (98.95%) | Memory: 44.8 MB (78.82%) - LeetSync

---
+++ 1-two-sum/two-sum.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[] sol = new int[2];
+        java.util.Map<Integer, Integer> map = new java.util.HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                sol[0] = map.get(complement);
+                sol[1] = i;
+                break;
+            }
+            map.put(nums[i], i);
+        }
+        return sol;
+    }
+}
