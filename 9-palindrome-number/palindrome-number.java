c61ac4fd0f506d61fc0c08ff0244f2fba3e61501
gNOR-mu <82237339+gNOR-mu@users.noreply.github.com>
2025-07-26 01:22:15 -0400

    Time: 5 ms (66.29%) | Memory: 43.8 MB (95.62%) - LeetSync

--- 9-palindrome-number/palindrome-number.java
+++ 9-palindrome-number/palindrome-number.java
@@ -1,8 +1,18 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        if(x<0){return false;}
-        StringBuilder sb = new StringBuilder();
-        sb.append(x);
-        return sb.toString().equals(sb.reverse().toString());
+        // los negativos no son palíndromos
+        if (x < 0) {
+            return false;
+        }
+
+        int copia = x;
+        int reverso = 0;
+
+        while (x > 0) {
+            reverso = (reverso * 10) + (x % 10);
+            x /= 10;
+        }
+
+        return reverso == copia;
     }
 }
