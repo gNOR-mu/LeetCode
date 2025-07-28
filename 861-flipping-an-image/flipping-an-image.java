2e45bd8fed05362758686b2b10db5fdcf78b21b4
gNOR-mu <82237339+gNOR-mu@users.noreply.github.com>
2025-07-28 02:19:20 -0400

    Time: 0 ms (100.00%) | Memory: 44.7 MB (40.08%) - LeetSync

---
+++ 861-flipping-an-image/flipping-an-image.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int[][] flipAndInvertImage(int[][] image) {
+        int[][] res = new int[image.length][image[0].length];
+        for (int i = 0; i < image.length; i++) {
+            int column = 0;
+            for (int j = image[0].length - 1; j >= 0; j--) {
+                res[i][column++] = image[i][j] == 0 ? 1 : 0;
+            }
+        }
+        return res;
+    }
+}
