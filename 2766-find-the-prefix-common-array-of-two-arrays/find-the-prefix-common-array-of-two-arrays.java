91ea86de0c955c20555848a5f94cb77cb3b4262a
gNOR-mu <82237339+gNOR-mu@users.noreply.github.com>
2025-07-27 21:45:22 -0400

    Time: 12 ms (21.84%) | Memory: 45.3 MB (98.39%) - LeetSync

--- 2766-find-the-prefix-common-array-of-two-arrays/find-the-prefix-common-array-of-two-arrays.java
+++ 2766-find-the-prefix-common-array-of-two-arrays/find-the-prefix-common-array-of-two-arrays.java
@@ -1,18 +1,21 @@
 class Solution {
     public int[] findThePrefixCommonArray(int[] A, int[] B) {
-        int[] solucion = new int[A.length];
-        Map<Integer, Integer> a = new HashMap<>();
-        Map<Integer, Integer> b = new HashMap<>();
+ int[] solucion = new int[A.length];
+        List<Integer> a = new ArrayList<>();
+        List<Integer> b = new ArrayList<>();
 
         int comunes = 0;
         for (int i = 0; i < A.length; i++) {
-            a.put(A[i], a.containsKey(A[i]) ? a.get(A[i]) : A[i]);
-            b.put(B[i], b.containsKey(B[i]) ? b.get(B[i]) : B[i]);
-
-            if (a.containsKey(B[i])) {
+            if (!a.contains(A[i])) {
+                a.add(A[i]);
+            }
+            if (!b.contains(B[i])) {
+                b.add(B[i]);
+            }
+            if (a.contains(B[i])) {
                 comunes++;
             }
-            if (b.containsKey(A[i]) && A[i] != B[i]) {
+            if (b.contains(A[i]) && A[i] != B[i]) {
                 comunes++;
             }
             solucion[i] = comunes;
