bd18683503a60bd37beb1e8682ab89cf13c1bbf9
gNOR-mu <82237339+gNOR-mu@users.noreply.github.com>
2025-07-28 19:18:49 -0400

    Time: 0 ms (100.00%) | Memory: 42 MB (18.84%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.java
+++ 58-length-of-last-word/length-of-last-word.java
@@ -4,15 +4,13 @@ import java.util.regex.Matcher;
 class Solution {
     public int lengthOfLastWord(String s) {
         int length = 0;
-        boolean isBlank = false;
         for (int i = s.length() - 1; i >= 0; i--) {
             char c = s.charAt(i);
-            if (c == ' ' && isBlank) {
+            if (c == ' ' && length>0) {
                 break;
             }
             if (c != ' ') {
                 length++;
-                isBlank = true;
             }
         }
         return length;
