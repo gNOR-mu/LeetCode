36275f32bfac688861be2dc613bd701333a64831
gNOR-mu <82237339+gNOR-mu@users.noreply.github.com>
2025-07-27 22:04:22 -0400

    Time: 0 ms (100.00%) | Memory: 42.7 MB (76.47%) - LeetSync

--- 1603-running-sum-of-1d-array/running-sum-of-1d-array.java
+++ 1603-running-sum-of-1d-array/running-sum-of-1d-array.java
@@ -1,9 +1,8 @@
 class Solution {
     public int[] runningSum(int[] nums) {
-        int[] sol = new int[nums.length];
-        for (int i = 0; i < nums.length; i++) {
-            sol[i] = i != 0 ? nums[i] + sol[i - 1] : nums[i];
+        for (int i = 1; i < nums.length; i++) {
+            nums[i] = nums[i - 1] + nums[i];
         }
-        return sol;
+        return nums;
     }
 }
