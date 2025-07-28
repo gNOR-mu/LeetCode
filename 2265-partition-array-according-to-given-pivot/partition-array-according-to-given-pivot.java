62c909bbde02e78735a541ab0d84467f1e1b141b
gNOR-mu <82237339+gNOR-mu@users.noreply.github.com>
2025-07-27 20:04:28 -0400

    Time: 4 ms (99.22%) | Memory: 59.1 MB (99.52%) - LeetSync

---
+++ 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int[] pivotArray(int[] nums, int pivot) {
+ int[] solucion = new int[nums.length];
+        int[] iguales = new int[nums.length];
+        int[] mayores = new int[nums.length];
+        int nMenores = 0;
+        int nMayores = 0;
+        int nIguales = 0;
+        for (int n : nums) {
+            if (n < pivot) {
+                solucion[nMenores] = n;
+                nMenores++;
+            } else if (n == pivot) {
+                iguales[nIguales] = n;
+                nIguales++;
+            } else {
+                mayores[nMayores] = n;
+                nMayores++;
+            }
+        }
+        System.arraycopy(iguales, 0, solucion, nMenores, nIguales);
+        System.arraycopy(mayores, 0, solucion, nMenores + nIguales, nMayores);
+
+        return solucion;
+    }
+}
