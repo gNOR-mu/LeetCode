9e4a4613ffc474ec321705a2c912e8c35860dd2f
gNOR-mu <82237339+gNOR-mu@users.noreply.github.com>
2025-07-28 20:28:34 -0400

    Time: 2 ms (17.86%) | Memory: 45.6 MB (66.76%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -1,21 +1,19 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
+        Set<Integer> rowWithZero = new HashSet<>();
         Set<Integer> colWithZero = new HashSet<>();
         for (int i = 0; i < matrix.length; i++) {
-            boolean mustBeZero = false;
             for (int j = 0; j < matrix[i].length; j++) {
                 if (matrix[i][j] == 0) {
-                    mustBeZero = true;
+                    rowWithZero.add(i);
                     colWithZero.add(j);
                 }
             }
-            if (mustBeZero) {
-                System.arraycopy(new int[matrix[i].length], 0, matrix[i], 0, matrix[i].length);
-            }
         }
         for (int i = 0; i < matrix.length; i++) {
+            boolean mustBeZero = rowWithZero.contains(i);
             for (int j = 0; j < matrix[0].length; j++) {
-                if (colWithZero.contains(j)) {
+                if (mustBeZero || colWithZero.contains(j)) {
                     matrix[i][j] = 0;
                 }
             }
