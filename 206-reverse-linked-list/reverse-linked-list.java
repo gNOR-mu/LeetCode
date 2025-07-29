e4baf758330e8cfccc55771b11fe2d6f66162899
gNOR-mu <82237339+gNOR-mu@users.noreply.github.com>
2025-07-29 17:53:54 -0400

    Time: 0 ms (100.00%) | Memory: 42.3 MB (92.73%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.java
+++ 206-reverse-linked-list/reverse-linked-list.java
@@ -10,12 +10,23 @@
  */
 class Solution {
     public ListNode reverseList(ListNode head) {
-        ListNode sol=null;
+        // ListNode sol=null;
 
-        while (head != null) {
-            sol = new ListNode(head.val, sol);
-            head = head.next;
+        // while (head != null) {
+        //     sol = new ListNode(head.val, sol);
+        //     head = head.next;
+        // }
+        // return sol;
+        if (head == null)
+            return null;
+        ListNode temp = head.next;
+        head.next = null;
+        while (temp != null) {
+            ListNode t2 = temp.next;
+            temp.next = head;
+            head = temp;
+            temp = t2;
         }
-        return sol;
+        return head;
     }
 }
