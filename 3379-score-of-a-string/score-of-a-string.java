c4ca4dced3614a51eeea54bd5d8e765cb35cd776
gNOR-mu <82237339+gNOR-mu@users.noreply.github.com>
2025-07-29 22:42:42 -0400

    Time: 1 ms (99.71%) | Memory: 42.2 MB (45.29%) - LeetSync

--- 3379-score-of-a-string/score-of-a-string.java
+++ 3379-score-of-a-string/score-of-a-string.java
@@ -1,7 +1,8 @@
 class Solution {
     public int scoreOfString(String s) {
         int score = 0;
-        for (int i = 1; i < s.length(); i++) {
+        int l = s.length();
+        for (int i = 1; i < l; i++) {
             score += Math.abs(s.charAt(i) - s.charAt(i - 1));
         }
         return score;
