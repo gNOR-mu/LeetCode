2e9b8b49883dab609c1427535730ac379987602b
gNOR-mu <82237339+gNOR-mu@users.noreply.github.com>
2025-07-31 02:09:44 -0400

    Time: 3 ms (94.86%) | Memory: 41.9 MB (99.50%) - LeetSync

---
+++ 2406-decode-the-message/decode-the-message.java
@@ -0,0 +1,24 @@
+class Solution {
+    public String decodeMessage(String key, String message) {
+        StringBuilder sb = new StringBuilder();
+        char[] decode = new char[26];
+        char start = 'a';
+
+        for (int i = 0; i < key.length(); i++) {
+            char c = key.charAt(i);
+            int val = c - 'a';
+            if (c != ' ' && decode[val] == '\u0000') {
+                decode[val] = start++;
+                if (start > 'z') {
+                    break;
+                }
+            }
+        }
+
+        for (int i = 0; i < message.length(); i++) {
+            char c = message.charAt(i);
+            sb.append(c != ' ' ? decode[c - 'a'] : c);
+        }
+        return sb.toString();
+    }
+}
