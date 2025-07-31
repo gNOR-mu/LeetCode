91316b62aeeb5ceadfbcf678639af48ae6d88673
gNOR-mu <82237339+gNOR-mu@users.noreply.github.com>
2025-07-31 19:33:39 -0400

    Time: 1 ms (20.32%) | Memory: 41.5 MB (64.34%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.java
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.java
@@ -1,6 +1,17 @@
 class Solution {
     public int strStr(String haystack, String needle) {
-        return haystack.indexOf(needle);
-
+        // return haystack.indexOf(needle); //ez
+        int s1l = haystack.length();
+        for (int i = 0; i < s1l; i++) {
+            for (int j = 0; j < needle.length() && i + j < s1l; j++) {
+                if (haystack.charAt(i + j) != needle.charAt(j)) {
+                    break;
+                }
+                if (j == needle.length() - 1) {
+                    return i;
+                }
+            }
+        }
+        return -1;
     }
 }
