af3ee18a54da6a0d55965ac2c17fd505dd88da77
gNOR-mu <82237339+gNOR-mu@users.noreply.github.com>
2025-07-31 02:59:44 -0400

    Time: 1 ms (99.20%) | Memory: 41.4 MB (7.03%) - LeetSync

--- 2752-sum-multiples/sum-multiples.java
+++ 2752-sum-multiples/sum-multiples.java
@@ -1,9 +1,21 @@
 class Solution {
     public static int sumOfMultiples(int n) {
+        boolean[] c = new boolean[n + 1];
+
+        for (int i = 3; i <= n; i += 3) {
+            c[i] = true;
+        }
+        for (int i = 5; i <= n; i += 5) {
+            c[i] = true;
+        }
+        for (int i = 7; i <= n; i += 7) {
+            c[i] = true;
+        }
         int sum = 0;
-        for (int i = 3; i <= n; i++) {
-            if (i % 3 == 0 || i % 5 == 0 || i % 7 == 0) {
+        for (int i = 1; i <= n; i++) {
+            if (c[i]) {
                 sum += i;
+
             }
         }
         return sum;
