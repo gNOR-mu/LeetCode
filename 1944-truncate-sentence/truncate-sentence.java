7907c186c0789c0c907034aee45742b2beb0f26b
gNOR-mu <82237339+gNOR-mu@users.noreply.github.com>
2025-07-30 23:23:28 -0400

    Time: 0 ms (100.00%) | Memory: 41.3 MB (98.80%) - LeetSync

--- 1944-truncate-sentence/truncate-sentence.java
+++ 1944-truncate-sentence/truncate-sentence.java
@@ -2,11 +2,11 @@ class Solution {
     public String truncateSentence(String s, int k) {
         int wordNum = 0;
         for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == ' ') {
+            if (s.charAt(i) == ' ' ) {
                 wordNum++;
-            }
-            if (k == wordNum) {
-                return s.substring(0, i);
+                if (k == wordNum) {
+                    return s.substring(0, i);
+                }
             }
         }
         return s;
