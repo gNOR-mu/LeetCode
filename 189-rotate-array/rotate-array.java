1f57f6fac26b0e3c5607734ed011fac1058dce07
gNOR-mu <82237339+gNOR-mu@users.noreply.github.com>
2025-08-01 15:39:52 -0400

    Time: 0 ms (100.00%) | Memory: 57.5 MB (52.25%) - LeetSync

--- 189-rotate-array/rotate-array.java
+++ 189-rotate-array/rotate-array.java
@@ -1,10 +1,20 @@
 class Solution {
     public void rotate(int[] nums, int k) {
+        //opt 1
         // int rotate = k%nums.length;
         // int[] aux = new int[nums.length];
         // System.arraycopy(nums, 0, aux, rotate, nums.length - rotate);
         // System.arraycopy(nums, nums.length - rotate, aux, 0, rotate);
         // System.arraycopy(aux, 0, nums, 0, nums.length);
+
+        //opt 2
+        // k = k % nums.length;
+        // int[] aux = new int[k];
+        // System.out.println(nums.length - k);
+        // System.arraycopy(nums, nums.length - k, aux, 0, k);
+        // System.arraycopy(nums, 0, nums, k, nums.length - k);
+        // System.arraycopy(aux, 0, nums, 0, k);
+
         k = k % nums.length;
         reverse(nums, 0, nums.length - 1);
         reverse(nums, 0, k - 1);
