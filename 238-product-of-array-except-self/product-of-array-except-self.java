097ae8fa999f693fcccfba797449a90ba5f0852b
gNOR-mu <82237339+gNOR-mu@users.noreply.github.com>
2025-08-02 17:19:56 -0400

    Time: 2 ms (87.37%) | Memory: 55.4 MB (61.81%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.java
+++ 238-product-of-array-except-self/product-of-array-except-self.java
@@ -1,17 +1,17 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        long total = 1;
+        int total = 1;
         int zeros = 0;
         for (int i = 0; i < nums.length; i++) {
             int num = nums[i];
             if (num != 0) {
                 total *= num;
             } else if (++zeros == 2) {
-                return new int[nums.length];
+                break;
             }
         }
-        if (nums.length == 1 && zeros == 1) {
-            return new int[1];
+        if (zeros > 1 || nums.length == 1 && zeros == 1) {
+            return new int[nums.length];
         }
 
         int[] product = new int[nums.length];
@@ -19,12 +19,12 @@ class Solution {
             int num = nums[i];
             if (num != 0) {
                 if (zeros == 0) {
-                    product[i] = (int) total / num;
+                    product[i] = total / num;
                 } else {
                     product[i] = 0;
                 }
             } else {
-                product[i] = (int) total;
+                product[i] = total;
             }
         }
         return product;
