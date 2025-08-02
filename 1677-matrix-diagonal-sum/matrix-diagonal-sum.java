7fd23d8cafa55905082607982f4390e597cc6cab
gNOR-mu <82237339+gNOR-mu@users.noreply.github.com>
2025-08-01 23:14:36 -0400

    Time: 0 ms (100.00%) | Memory: 43.9 MB (98.41%) - LeetSync

---
+++ 1677-matrix-diagonal-sum/matrix-diagonal-sum.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int diagonalSum(int[][] mat) {
+        int sum = 0;
+        if (mat.length == 0) {
+            return mat[0][0];
+        }
+        if ((mat.length & 1) == 1) {
+            sum -= mat[mat.length / 2][mat.length / 2];
+        }
+        for (int i = 0; i < mat.length; i++) {
+            sum += mat[i][i] + mat[mat.length - 1 - i][i];
+        }
+        return sum;
+    }
+}
