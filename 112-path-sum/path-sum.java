e1a1920d26b8fbc03ca11bf534319f7bf3aabdd8
gNOR-mu <82237339+gNOR-mu@users.noreply.github.com>
2025-08-03 19:09:37 -0400

    Time: 0 ms (100.00%) | Memory: 43.2 MB (60.61%) - LeetSync

--- 112-path-sum/path-sum.java
+++ 112-path-sum/path-sum.java
@@ -18,10 +18,10 @@ class Solution {
         if (root == null) {
             return false;
         }
-        if (root.left == null && root.right == null) {
-            return targetSum == root.val;
-        }
         targetSum -= root.val;
+        if (root.left == null && root.right == null) {
+            return targetSum == 0;
+        }
         return hasPathSum(root.left, targetSum) || hasPathSum(root.right, targetSum);
     }
 }
