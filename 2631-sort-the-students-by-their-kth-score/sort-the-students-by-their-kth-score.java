c78633e067445cd93156094ec1e57daa9712ead7
gNOR-mu <82237339+gNOR-mu@users.noreply.github.com>
2025-08-08 15:08:52 -0400

    Time: 2 ms (97.07%) | Memory: 52.6 MB (31.15%) - LeetSync

--- 2631-sort-the-students-by-their-kth-score/sort-the-students-by-their-kth-score.java
+++ 2631-sort-the-students-by-their-kth-score/sort-the-students-by-their-kth-score.java
@@ -1,18 +1,15 @@
 class Solution {
     public int[][] sortTheStudents(int[][] score, int k) {
-     for(int a=0;a<score.length-1;a++){
-         for(int i=0;i<score.length-1;i++){
-               if(score[i][k] < score[i+1][k]) swapNext(score,i);
+        for (int i = 0; i < score.length; i++) {
+            int index = i;
+            for (int j = i + 1; j < score.length; j++) {
+                if (score[j][k] > score[index][k])
+                    index = j;
+            }
+            int[] temp = score[i];
+            score[i] = score[index];
+            score[index] = temp;
         }
-     }
         return score;
-    }
-
-    void swapNext(int[][] score, int i) {
-        for (int j = 0; j < score[i].length; j++) {
-            int tmp = score[i][j];
-            score[i][j] = score[i + 1][j];
-            score[i + 1][j] = tmp;
-        }
     }
 }
