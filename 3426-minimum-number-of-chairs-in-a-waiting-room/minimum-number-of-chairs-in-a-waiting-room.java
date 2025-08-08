888fbe327bb1e035356f9e06b2455bebdfa88d0f
gNOR-mu <82237339+gNOR-mu@users.noreply.github.com>
2025-08-08 16:08:30 -0400

    Time: 1 ms (100.00%) | Memory: 42.6 MB (13.11%) - LeetSync

--- 3426-minimum-number-of-chairs-in-a-waiting-room/minimum-number-of-chairs-in-a-waiting-room.java
+++ 3426-minimum-number-of-chairs-in-a-waiting-room/minimum-number-of-chairs-in-a-waiting-room.java
@@ -3,12 +3,9 @@ class Solution {
         int max = 0;
         int ocupadas = 0;
         for (int i = 0; i < s.length(); i++) {
-            if(s.charAt(i) == 'E'){
-                ocupadas++;
-            }else{
+            if (s.charAt(i) == 'L') {
                 ocupadas--;
-            }
-            if(ocupadas >max){
+            } else if (++ocupadas > max) {
                 max = ocupadas;
             }
         }
