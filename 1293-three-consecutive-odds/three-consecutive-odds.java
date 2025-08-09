efe08b5649bf3c4e4318aa657c6487d71ad28f87
gNOR-mu <82237339+gNOR-mu@users.noreply.github.com>
2025-08-08 21:31:47 -0400

    Time: 0 ms (100.00%) | Memory: 42.7 MB (13.66%) - LeetSync

--- 1293-three-consecutive-odds/three-consecutive-odds.java
+++ 1293-three-consecutive-odds/three-consecutive-odds.java
@@ -1,12 +1,11 @@
 class Solution {
     public boolean threeConsecutiveOdds(int[] arr) {
         for (int i = 0; i < arr.length - 2; i++) {
-            if ((arr[i] & 1) == 0
-                    || (arr[++i] & 1) == 0
-                    || (arr[++i] & 1) == 0) {
-                continue;
+            if ((arr[i] & 1) == 1
+                    && (arr[++i] & 1) == 1
+                    && (arr[++i] & 1) == 1) {
+                return true;
             }
-            return true;
         }
         return false;
     }
