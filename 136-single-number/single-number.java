c72758e2b4c8228215d78e236d28c95a73130346
gNOR-mu <82237339+gNOR-mu@users.noreply.github.com>
2025-08-09 18:55:58 -0400

    Time: 1 ms (99.85%) | Memory: 46.4 MB (26.04%) - LeetSync

--- 136-single-number/single-number.java
+++ 136-single-number/single-number.java
@@ -1,13 +1,10 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        Arrays.sort(nums);
-        for(int i=0;i< nums.length-1;i+=2){
-            if(nums[i] != nums[i+1]){
-                return nums[i];
-            }
+        int r = 0;
+        for (int i = 0; i < nums.length; i++) {
+            r ^= nums[i];
         }
-        return nums[nums.length-1];
+        return r;
     }
 
-  
 }
