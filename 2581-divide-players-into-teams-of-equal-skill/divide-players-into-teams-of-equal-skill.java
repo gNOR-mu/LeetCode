578a2dd6d0134cd3c478e967c1787c8e732fcf80
gNOR-mu <82237339+gNOR-mu@users.noreply.github.com>
2025-08-08 22:14:01 -0400

    Time: 16 ms (55.27%) | Memory: 57.6 MB (60.62%) - LeetSync

--- 2581-divide-players-into-teams-of-equal-skill/divide-players-into-teams-of-equal-skill.java
+++ 2581-divide-players-into-teams-of-equal-skill/divide-players-into-teams-of-equal-skill.java
@@ -2,12 +2,12 @@ class Solution {
     public long dividePlayers(int[] skill) {
         Arrays.sort(skill);
         long res = 0;
-        float avg = 0;
+        int avg = 0;
         int mid = skill.length / 2;
         for (int i = 0; i < mid; i++) {
             int actual = skill[i];
             int par = skill[skill.length - 1 - i];
-            float avgTeam = (actual + par) / 2f;
+            int avgTeam = actual + par;
             if (avg != 0 && avg != avgTeam) {
                 return -1;
             } else {
