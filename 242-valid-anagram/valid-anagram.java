0115f051805b7e8101ed5ab9462c3d981c2baa60
gNOR-mu <82237339+gNOR-mu@users.noreply.github.com>
2025-08-11 00:24:27 -0400

    Time: 4 ms (77.35%) | Memory: 42.9 MB (88.64%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,9 +1,20 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        char[] s1 = s.toCharArray();
-        char[] t1 = t.toCharArray();
-        Arrays.sort(s1);
-        Arrays.sort(t1);
-        return Arrays.equals(s1, t1);
+        if(s.length()!=t.length()){
+            return false;
+        }
+        int[] dict = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            dict[s.charAt(i) - 'a'] += 1;
+        }
+        for (int i = 0; i < t.length(); i++) {
+            dict[t.charAt(i) - 'a'] -= 1;
+        }
+        for (int i = 0; i < 26; i++) {
+            if (dict[i] != 0) {
+                return false;
+            }
+        }
+        return true;
     }
 }
