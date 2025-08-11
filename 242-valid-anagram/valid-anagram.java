afb90275790f61263153a94ce2c56b007e228795
gNOR-mu <82237339+gNOR-mu@users.noreply.github.com>
2025-08-11 00:54:39 -0400

    Time: 3 ms (92.00%) | Memory: 44.8 MB (34.61%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -7,11 +7,6 @@ class Solution {
         char[] t1 = t.toCharArray();
         Arrays.sort(s1);
         Arrays.sort(t1);
-        for (int i = 0; i < s.length(); i++) {
-            if (t1[i] != s1[i]) {
-                return false;
-            }
-        }
-        return true;
+       return Arrays.equals(s1, t1);
     }
 }
