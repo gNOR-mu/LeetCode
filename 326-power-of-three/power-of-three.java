9b93329289bf17562c379fa282943e6d59303138
gNOR-mu <82237339+gNOR-mu@users.noreply.github.com>
2025-08-11 15:07:43 -0400

    Time: 8 ms (84.51%) | Memory: 44.3 MB (27.76%) - LeetSync

--- 326-power-of-three/power-of-three.java
+++ 326-power-of-three/power-of-three.java
@@ -1,14 +1,9 @@
 class Solution {
     public boolean isPowerOfThree(int n) {
-        // if(n<1){
-        //     return false;
-        // }
-        // double p = Math.log10(n) / Math.log10(3);
-        // return (int)p==p;
-        if (n < 1 || (n&1)==0) {
+        if(n<1){
             return false;
         }
-        
-        return 1_162_261_467 % n == 0;
+        double p = Math.log10(n) / Math.log10(3);
+        return (int)p==p;
     }
 }
