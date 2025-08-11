af0504baed58793568aaaec2ce45640fb9c8e1c8
gNOR-mu <82237339+gNOR-mu@users.noreply.github.com>
2025-08-11 01:44:29 -0400

    Time: 4 ms (77.35%) | Memory: 44.5 MB (50.99%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,8 +1,5 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if(s.length() != t.length()){
-            return false;
-        }
         char[] s1 = s.toCharArray();
         char[] t1 = t.toCharArray();
         Arrays.sort(s1);
