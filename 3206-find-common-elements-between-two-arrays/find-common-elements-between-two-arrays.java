93881b2ecf53fafedbe25a7e680648e1435148bb
gNOR-mu <82237339+gNOR-mu@users.noreply.github.com>
2025-08-12 21:16:21 -0400

    Time: 2 ms (96.92%) | Memory: 45.6 MB (63.77%) - LeetSync

---
+++ 3206-find-common-elements-between-two-arrays/find-common-elements-between-two-arrays.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] findIntersectionValues(int[] nums1, int[] nums2) {
+        int[] f1 = new int[101];
+        int[] f2 = new int[101];
+        int match = 0;
+        int[] res = new int[2];
+        for (int i = 0; i < nums1.length; i++) {
+            f1[nums1[i]]++;
+        }
+        for (int i = 0; i < nums2.length; i++) {
+            f2[nums2[i]]++;
+        }
+        for (int i = 0; i < 101; i++) {
+            if (f1[i] > 0 && f2[i]>0) {
+                res[0] +=f1[i];
+                res[1] +=f2[i];
+            }
+        }
+        return res;
+    }
+}
