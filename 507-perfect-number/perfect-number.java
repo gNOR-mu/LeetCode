13195c031b595e551e138de4312ba97399ebfc6a
gNOR-mu <82237339+gNOR-mu@users.noreply.github.com>
2025-08-13 02:27:55 -0400

    Time: 2 ms (79.90%) | Memory: 40.4 MB (73.37%) - LeetSync

--- 507-perfect-number/perfect-number.java
+++ 507-perfect-number/perfect-number.java
@@ -1,9 +1,13 @@
 class Solution {
     public boolean checkPerfectNumber(int num) {
-        int sum = 0;
-        for (int i = 1; i <= num / 2; i++) {
-            if (num % i == 0) {
-                sum+=i;
+                if(num <= 1) return false;
+        int sum = 1;
+        for(int i = 2; i <= Math.sqrt(num); i++){
+            if(num % i == 0){
+                sum += i;
+                if(i != num / i){
+                    sum += num / i;
+                }
             }
         }
         return sum == num;
