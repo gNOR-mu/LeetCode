1a981a7200e7b016f3dbc7a14326d173f5739f10
gNOR-mu <82237339+gNOR-mu@users.noreply.github.com>
2025-08-13 18:05:33 -0400

    Time: 9 ms (99.35%) | Memory: 61 MB (10.30%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.java
+++ 217-contains-duplicate/contains-duplicate.java
@@ -7,6 +7,8 @@ class Solution {
         //     }
         // }
         // return false;
+
+        //opt 2
         Set<Integer> nums2 = new HashSet<>();
         for (int i = 0; i < nums.length; i++) {
             if (!nums2.add(nums[i])) {
@@ -14,5 +16,35 @@ class Solution {
             }
         }
         return false;
+    }
+
+    public void quickSort(int arr[], int begin, int end) {
+        if (begin < end) {
+            int partitionIndex = partition(arr, begin, end);
+
+            quickSort(arr, begin, partitionIndex - 1);
+            quickSort(arr, partitionIndex + 1, end);
+        }
+    }
+
+    private int partition(int arr[], int begin, int end) {
+        int pivot = arr[end];
+        int i = (begin - 1);
+
+        for (int j = begin; j < end; j++) {
+            if (arr[j] <= pivot) {
+                i++;
+
+                int swapTemp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = swapTemp;
+            }
+        }
+
+        int swapTemp = arr[i + 1];
+        arr[i + 1] = arr[end];
+        arr[end] = swapTemp;
+
+        return i + 1;
     }
 }
