880204fd8b705d83623291e9889813c7f7bc38a9
gNOR-mu <82237339+gNOR-mu@users.noreply.github.com>
2025-08-13 01:53:09 -0400

    Time: 0 ms (100.00%) | Memory: 40.3 MB (80.65%) - LeetSync

---
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int fib(int n) {
+        int n1 = 0, n2 = 1;
+        int n3;
+        for(int i=0;i<n;i++){
+            n3 = n1 + n2;
+            n1=n2;
+            n2=n3;
+        }
+        return n1;
+    }
+}
