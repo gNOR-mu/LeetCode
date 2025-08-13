3c8b73d1e8b51129a0f3c9cf19f8808865c8923e
gNOR-mu <82237339+gNOR-mu@users.noreply.github.com>
2025-08-12 20:37:01 -0400

    Time: 3 ms (29.99%) | Memory: 97.9 MB (6.51%) - LeetSync

---
+++ 1916-find-center-of-star-graph/find-center-of-star-graph.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int findCenter(int[][] edges) {
+        int[] f = new int[edges.length * 2];
+        int max = 0;
+        int idx =0;
+        for (int i = 0; i < edges.length; i++) {
+            f[edges[i][0]]++;
+            f[edges[i][1]]++;
+        }
+        for (int i = 0; i < f.length; i++) {
+            if(f[i] >max){
+                max = f[i];
+                idx = i;
+            }
+        }
+        return idx;
+    }
+}
