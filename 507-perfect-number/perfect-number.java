6b240b9417cf56f4e4716f45a83f1be16929c1f8
gNOR-mu <82237339+gNOR-mu@users.noreply.github.com>
2025-08-13 02:29:11 -0400

    Time: 2 ms (79.90%) | Memory: 40.2 MB (86.51%) - LeetSync

--- 507-perfect-number/perfect-number.java
+++ 507-perfect-number/perfect-number.java
@@ -5,8 +5,7 @@ class Solution {
         int sum = 1;
         for (int i = 2; i <= Math.sqrt(num); i++) {
             if (num % i == 0) {
-                sum += i;
-                sum += num / i;
+                sum += (num / i) + i;
             }
         }
         return sum == num;
