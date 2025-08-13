fab64a2c3452403a75fe7bb2a5ba785535d298f7
gNOR-mu <82237339+gNOR-mu@users.noreply.github.com>
2025-08-12 21:27:55 -0400

    Time: 2 ms (62.66%) | Memory: 41.5 MB (89.83%) - LeetSync

--- 2401-count-asterisks/count-asterisks.java
+++ 2401-count-asterisks/count-asterisks.java
@@ -1,13 +1,12 @@
 class Solution {
     public int countAsterisks(String s) {
-        int bars = 0;
+        boolean validBar = true;;
         int count = 0;
         for(int i=0;i<s.length();i++){
             char c = s.charAt(i);
             if(c == '|'){
-                bars++;
-            }
-            if(c=='*'&& (bars&1)==0){
+                validBar = !validBar;
+            }else if(c=='*'&& validBar){
                 count++;
             }
         }
