ce6b29621f4a6cdf100451f5fc44eaf119e1ec85
gNOR-mu <82237339+gNOR-mu@users.noreply.github.com>
2025-08-14 23:05:16 -0400

    Time: 0 ms (100.00%) | Memory: 41.4 MB (8.89%) - LeetSync

--- 342-power-of-four/power-of-four.java
+++ 342-power-of-four/power-of-four.java
@@ -1,6 +1,6 @@
 class Solution {
     public boolean isPowerOfFour(int n) {
-        if(n<1){
+        if(n<1 || n>1 && (n&1)==1){
             return false;
         }
         double p = Math.log10(n) / 0.6020599913279624; // 0.6020599913279624 log10(4)
