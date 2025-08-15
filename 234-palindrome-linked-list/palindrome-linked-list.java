86412bfe5727511809918b0ace51c9893fa1101b
gNOR-mu <82237339+gNOR-mu@users.noreply.github.com>
2025-08-15 18:23:54 -0400

    Time: 5 ms (55.02%) | Memory: 58.3 MB (81.67%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.java
+++ 234-palindrome-linked-list/palindrome-linked-list.java
@@ -10,12 +10,36 @@
  */
 class Solution {
     public boolean isPalindrome(ListNode head) {
-        StringBuilder sb = new StringBuilder();
-        ListNode tmp = head;
-        while(tmp!=null){
-            sb.append(tmp.val);
-            tmp = tmp.next;
+        // StringBuilder sb = new StringBuilder();
+        // ListNode tmp = head;
+        // while(tmp!=null){
+        //     sb.append(tmp.val);
+        //     tmp = tmp.next;
+        // }
+        // return sb.toString().equals(sb.reverse().toString());
+        ListNode fast = head;
+        ListNode slow = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
         }
-        return sb.toString().equals(sb.reverse().toString());
+
+        ListNode slow2 = slow;
+        ListNode invert = null;
+        while (slow2 != null) {
+            invert = new ListNode(slow2.val, invert);
+            slow2 = slow2.next;
+        }
+
+        ListNode h2 = head;
+        while (h2 != slow && invert != null) {
+            if (h2.val != invert.val) {
+                return false;
+            }
+            h2 = h2.next;
+            invert = invert.next;
+        }
+        return true;
     }
 }
