87a13119a5825bc3cb5fdd13a26db8d36ad1ee87
gNOR-mu <82237339+gNOR-mu@users.noreply.github.com>
2025-08-14 23:35:35 -0400

    Time: 0 ms (100.00%) | Memory: 40.1 MB (93.35%) - LeetSync

--- 2481-strictly-palindromic-number/strictly-palindromic-number.java
+++ 2481-strictly-palindromic-number/strictly-palindromic-number.java
@@ -1,21 +1,23 @@
 class Solution {
     public static boolean isStrictlyPalindromic(int n) {
-        for (int i = 2; i <= n - 2; i++) {
-            if (!basePalindromic(i, n)) {
-                return false;
-            }
-        }
-        return true;
+        return n==2 || n==3;
+
+        // for (int i = 2; i <= n - 2; i++) {
+        //     if (!basePalindromic(i, n)) {
+        //         return false;
+        //     }
+        // }
+        // return true;
     }
 
-    static boolean basePalindromic(int base, int n) {
-        StringBuilder sb = new StringBuilder();
-        while (n > 0) {
-            int mod = n % base;
-            n /= base;
-            sb.append(mod);
-        }
-        String s = sb.toString();
-        return s.equals(sb.reverse().toString());
-    }
+    // static boolean basePalindromic(int base, int n) {
+    //     StringBuilder sb = new StringBuilder();
+    //     while (n > 0) {
+    //         int mod = n % base;
+    //         n /= base;
+    //         sb.append(mod);
+    //     }
+    //     String s = sb.toString();
+    //     return s.equals(sb.reverse().toString());
+    // }
 }
