f9884db2c28c95bef2ee3699620219230b17939e
gNOR-mu <82237339+gNOR-mu@users.noreply.github.com>
2025-08-16 23:12:17 -0400

    Time: 1 ms (84.73%) | Memory: 43.5 MB (26.38%) - LeetSync

--- 2210-find-target-indices-after-sorting-array/find-target-indices-after-sorting-array.java
+++ 2210-find-target-indices-after-sorting-array/find-target-indices-after-sorting-array.java
@@ -1,7 +1,7 @@
 class Solution {
     public List<Integer> targetIndices(int[] nums, int target) {
         List<Integer> res = new ArrayList<>();
-        Arrays.sort(nums);
+        quickSort(nums,0,nums.length-1);
         for(int i=0;i<nums.length;i++){
             if(nums[i] == target){
                 res.add(i);
@@ -10,6 +10,32 @@ class Solution {
             }
         }
         return res;
+    }
+    
+    static int partition(int[] arr, int low, int high) {
+        int pivot = arr[high];
+        int i = low - 1;
+        for (int j = low; j <= high - 1; j++) {
+            if (arr[j] < pivot) {
+                i++;
+                swap(arr, i, j);
+            }
+        }
+        swap(arr, i + 1, high);  
+        return i + 1;
+    }
 
+    static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    static void quickSort(int[] arr, int low, int high) {
+        if (low < high) {
+            int pi = partition(arr, low, high);
+            quickSort(arr, low, pi - 1);
+            quickSort(arr, pi + 1, high);
+        }
     }
 }
