931613f3c6abbcfa89dc52968614f10eb8cab32d
gNOR-mu <82237339+gNOR-mu@users.noreply.github.com>
2025-08-17 18:26:58 -0400

    Time: 0 ms (100.00%) | Memory: 41.2 MB (31.08%) - LeetSync

--- 258-add-digits/add-digits.java
+++ 258-add-digits/add-digits.java
@@ -1,13 +1,14 @@
 class Solution {
     public int addDigits(int num) {
-        if(num<10){
-            return num;
-        }
-        int sum = 0;
-        while(num!=0){
-            sum += num%10;
-            num /=10;
-        }
-        return addDigits(sum);
+        // if(num<10){
+        //     return num;
+        // }
+        // int sum = 0;
+        // while(num!=0){
+        //     sum += num%10;
+        //     num /=10;
+        // }
+        // return addDigits(sum);
+        return (num-1)%9+1;
     }
 }
