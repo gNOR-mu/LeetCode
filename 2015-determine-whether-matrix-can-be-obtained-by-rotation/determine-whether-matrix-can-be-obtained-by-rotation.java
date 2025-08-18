beb2837861ccc1477958e9ab2648bbe3a8c2e6f1
gNOR-mu <82237339+gNOR-mu@users.noreply.github.com>
2025-08-17 22:15:49 -0400

    Time: 0 ms (100.00%) | Memory: 42.1 MB (24.01%) - LeetSync

---
+++ 2015-determine-whether-matrix-can-be-obtained-by-rotation/determine-whether-matrix-can-be-obtained-by-rotation.java
@@ -0,0 +1,38 @@
+class Solution {
+    public boolean findRotation(int[][] mat, int[][] target) {
+        rotate(mat);
+        if (Arrays.deepEquals(mat, target)){
+            return true;
+        }
+        rotate(mat);
+        if (Arrays.deepEquals(mat, target)){
+            return true;
+        }
+        rotate(mat);
+        if (Arrays.deepEquals(mat, target)){
+            return true;
+        }
+        rotate(mat);
+        if (Arrays.deepEquals(mat, target)){
+            return true;
+        }
+        return false;
+    }
+
+    public static void rotate(int[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = i; j < matrix[i].length; j++) {
+                int tmp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = tmp;
+            }
+        }
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length / 2; j++) {
+                int tmp = matrix[i][j];
+                matrix[i][j] = matrix[i][matrix[i].length - 1 - j];
+                matrix[i][matrix[i].length - 1 - j] = tmp;
+            }
+        }
+    }
+}
