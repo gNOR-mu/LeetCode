b82c6077e955fcb42dde90a3bf5eedd57177b5f7
gNOR-mu <82237339+gNOR-mu@users.noreply.github.com>
2025-08-18 16:03:36 -0400

    Time: 2 ms (93.85%) | Memory: 46.8 MB (50.89%) - LeetSync

--- 338-counting-bits/counting-bits.java
+++ 338-counting-bits/counting-bits.java
@@ -1,9 +1,8 @@
 class Solution {
     public int[] countBits(int n) {
         int[] res = new int[n + 1];
-        int start = 0;
         for (int i = 1; i < res.length; i++) {
-            res[i] = Integer.bitCount(i);
+            res[i] = res[i >> 1] + (i & 1);
         }
         return res;
     }
