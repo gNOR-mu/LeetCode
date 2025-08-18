ca8a22061ff0f2b106bd68bc12ed71ee0c81614c
gNOR-mu <82237339+gNOR-mu@users.noreply.github.com>
2025-08-18 18:07:12 -0400

    Time: 1 ms (99.53%) | Memory: 42 MB (46.88%) - LeetSync

--- 389-find-the-difference/find-the-difference.java
+++ 389-find-the-difference/find-the-difference.java
@@ -1,13 +1,21 @@
 class Solution {
     public char findTheDifference(String s, String t) {
-        int a = 0;
-        int b = 0;
+        // int a = 0;
+        // int b = 0;
+        // for (char c : s.toCharArray()) {
+        //     a += c;
+        // }
+        // for (char c : t.toCharArray()) {
+        //     b += c;
+        // }
+        // return s.length() > t.length() ? (char) (a - b) : (char) (b - a);
+        char res = 0;
         for (char c : s.toCharArray()) {
-            a += c;
+            res ^=(char)c;
         }
         for (char c : t.toCharArray()) {
-            b += c;
+            res ^=(char)c;
         }
-        return s.length() > t.length() ? (char) (a - b) : (char) (b - a);
+        return res;
     }
 }
