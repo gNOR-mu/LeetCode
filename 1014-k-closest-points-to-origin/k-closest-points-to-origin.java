9fc194e38f74093b95b590c16d5be1dc030fe787
gNOR-mu <82237339+gNOR-mu@users.noreply.github.com>
2025-08-19 02:14:42 -0400

    Time: 21 ms (92.94%) | Memory: 48.2 MB (95.24%) - LeetSync

--- 1014-k-closest-points-to-origin/k-closest-points-to-origin.java
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.java
@@ -1,23 +1,16 @@
 class Solution {
     public int[][] kClosest(int[][] points, int k) {
-        if (k == points.length) {
-            return points;
-        }
+        Comparator<int[]> comparator = (a, b) -> (a[0] * a[0] + a[1] * a[1]) - (b[0] * b[0] + b[1] * b[1]);
+        PriorityQueue<int[]> pq = new PriorityQueue<>(comparator);
         int[][] res = new int[k][2];
-        Map<Integer, Double> map = new HashMap<>();
+
         for (int i = 0; i < points.length; i++) {
-            int[] p = points[i];
-            map.put(i, Math.sqrt(p[0] * p[0] + p[1] * p[1]));
+            pq.add(points[i]);
         }
-        var sorted = map.entrySet()
-                .stream()
-                .sorted(Map.Entry.comparingByValue())
-                .toList();
         for (int i = 0; i < k; i++) {
-            Map.Entry<Integer, Double> entry = sorted.get(i);
-            int[] p = points[entry.getKey()];
-            res[i][0] = p[0];
-            res[i][1] = p[1];
+            int[] r = pq.poll();
+            res[i][0] = r[0];
+            res[i][1] = r[1];
         }
         return res;
     }
