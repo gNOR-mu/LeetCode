1b5ced90fca5934ac9914239a7cb3cefa0b504a8
gNOR-mu <82237339+gNOR-mu@users.noreply.github.com>
2025-08-20 03:20:37 -0400

    Time: 1 ms (100.00%) | Memory: 57.1 MB (67.49%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.java
+++ 41-first-missing-positive/first-missing-positive.java
@@ -1,17 +1,26 @@
 class Solution {
     public static int firstMissingPositive(int[] nums) {
-      for (int i = 0; i < nums.length; i++) {
-            while (nums[i] > 0 && nums[i] <= nums.length && nums[nums[i] - 1] != nums[i]) {
-                int temp = nums[nums[i] - 1];
-                nums[nums[i] - 1] = nums[i];
-                nums[i] = temp;
+        int end = nums.length;
+        //leet code cycle sort
+        int i=0;
+        while (i < end) {
+            int correctIdx = nums[i] - 1;
+            if (nums[i] > 0 && nums[i] <= end && nums[i] != nums[correctIdx]) {
+                swap(nums, i, correctIdx);
+            } else {
+                i++;
             }
         }
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] != i + 1) {
+        for (i = 0; i < nums.length; i++) {
+           if (nums[i] != i + 1) {
                 return i + 1;
             }
         }
-        return nums.length + 1;
+        return end+1;
+    }
+   private static void swap(int[] nums, int index1, int index2) {
+        int temp = nums[index1];
+        nums[index1] = nums[index2];
+        nums[index2] = temp;
     }
 }
