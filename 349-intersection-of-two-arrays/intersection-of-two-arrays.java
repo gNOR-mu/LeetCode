5910933b4edb16797859422b7db0c185afb3c8ca
gNOR-mu <82237339+gNOR-mu@users.noreply.github.com>
2025-08-20 16:07:10 -0400

    Time: 2 ms (96.86%) | Memory: 43.6 MB (35.30%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.java
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.java
@@ -1,7 +1,20 @@
 class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
-        Set<Integer> set = Arrays.stream(nums1).boxed().collect(Collectors.toSet());
-        set.retainAll(Arrays.stream(nums2).boxed().collect(Collectors.toSet()));
-        return set.stream().mapToInt(Number::intValue).toArray();
+        Set<Integer> set = new HashSet<>();
+        Set<Integer> set2 = new HashSet<>();
+        for (int i = 0; i < nums1.length; i++) {
+            set.add(nums1[i]);
+        }
+        for (int i = 0; i < nums2.length; i++) {
+            if (set.contains(nums2[i])) {
+                set2.add(nums2[i]);
+            }
+        }
+        int[] res = new int[set2.size()];
+        int i = 0;
+        for (var s2 : set2) {
+            res[i++] = s2;
+        }
+        return res;
     }
 }
