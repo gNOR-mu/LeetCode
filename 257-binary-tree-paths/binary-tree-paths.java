bec10dae0d7708d8cd21a0778cddd42ef57d8c29
gNOR-mu <82237339+gNOR-mu@users.noreply.github.com>
2025-08-20 23:09:43 -0400

    Time: 1 ms (98.36%) | Memory: 42.5 MB (78.62%) - LeetSync

--- 257-binary-tree-paths/binary-tree-paths.java
+++ 257-binary-tree-paths/binary-tree-paths.java
@@ -17,21 +17,21 @@ class Solution {
 
     public List<String> binaryTreePaths(TreeNode root) {
         List<String> list = new ArrayList<>();
-        paths(root, list, "");
+        paths(root, list, new StringBuilder());
         return list;
     }
 
-    public void paths(TreeNode root, List<String> list, String path) {
+    public void paths(TreeNode root, List<String> list, StringBuilder path) {
         if (root == null) {
             return;
         }
-        path += root.val;
+        path.append(root.val);
         if (root.left == null && root.right == null) {
-            list.add(path);
+            list.add(path.toString());
         }
-        path += "->";
-        paths(root.left, list, path);
-        paths(root.right, list, path);
+        path.append("->");
+        paths(root.left, list, new StringBuilder(path));
+        paths(root.right, list, new StringBuilder(path));
     }
 
 }
