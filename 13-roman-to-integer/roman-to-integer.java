ceb103496481f2079beaee931cb6380fcc3921a5
gNOR-mu <82237339+gNOR-mu@users.noreply.github.com>
2025-08-22 00:16:53 -0400

    Time: 7 ms (8.36%) | Memory: 44.9 MB (34.55%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.java
+++ 13-roman-to-integer/roman-to-integer.java
@@ -1,36 +1,24 @@
 class Solution {
     public int romanToInt(String s) {
         int num = 0;
-        Map<Character, Integer> map = new HashMap<>();
-        map.put('I', 1);
-        map.put('V', 5);
-        map.put('X', 10);
-        map.put('L', 50);
-        map.put('C', 100);
-        map.put('D', 500);
-        map.put('M', 1000);
-
-        Map<String, Integer> comp = new HashMap();
-        comp.put("IV", 4);
-        comp.put("IX", 9);
-        comp.put("XL", 40);
-        comp.put("XC", 90);
-        comp.put("CD", 400);
-        comp.put("CM", 900);
+        Map<Character, Integer> map = Map.of(
+                'I', 1,
+                'V', 5,
+                'X', 10,
+                'L', 50,
+                'C', 100,
+                'D', 500,
+                'M', 1000);
 
         for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-            if (i < s.length() - 1) {
-                String next = c + "" + s.charAt(i + 1);
-                if (comp.containsKey(next)) {
-                    num += comp.get(next);
-                    i++;
-                    continue;
-                }
+            int actual = map.get(s.charAt(i));
+            int next = (i < s.length() - 1) ? map.get(s.charAt(i + 1)) : 0;
+
+            if (actual < next) {
+                num -= actual;
+            } else {
+                num += actual;
             }
-
-            num += map.get(c);
-
         }
         return num;
     }
