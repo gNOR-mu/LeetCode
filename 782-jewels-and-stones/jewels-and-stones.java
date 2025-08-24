a17e6f0e0e29505f85cc703b806e8fd2d0b77947
gNOR-mu <82237339+gNOR-mu@users.noreply.github.com>
2025-08-24 02:46:02 -0400

    Time: 0 ms (100.00%) | Memory: 41.8 MB (79.21%) - LeetSync

--- 782-jewels-and-stones/jewels-and-stones.java
+++ 782-jewels-and-stones/jewels-and-stones.java
@@ -1,12 +1,8 @@
 class Solution {
     public int numJewelsInStones(String jewels, String stones) {
-        Set<Character> set = new HashSet<>();
         int res = 0;
-        for(char j:jewels.toCharArray()){
-            set.add(j);
-        }
         for(char s:stones.toCharArray()){
-            if(set.contains(s)){
+            if(jewels.indexOf(s)!=-1){
                 res++;
             }
         }
