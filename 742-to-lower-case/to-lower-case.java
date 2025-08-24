1dd2fc2ba865c662422b1635a7a041d46493c4a8
gNOR-mu <82237339+gNOR-mu@users.noreply.github.com>
2025-08-24 02:27:41 -0400

    Time: 0 ms (100.00%) | Memory: 41.5 MB (78.95%) - LeetSync

--- 742-to-lower-case/to-lower-case.java
+++ 742-to-lower-case/to-lower-case.java
@@ -1,5 +1,14 @@
 class Solution {
     public String toLowerCase(String s) {
-        return s.toLowerCase();
+        // return s.toLowerCase();
+        StringBuilder sb = new StringBuilder();
+        for (char c : s.toCharArray()) {
+            if (c >= 65 && c <= 90) {
+                c += 32;
+            } 
+
+                sb.append(c);
+        }
+        return sb.toString();
     }
 }
