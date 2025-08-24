200b2138a08920e1f55d021c3609530137fa6eea
gNOR-mu <82237339+gNOR-mu@users.noreply.github.com>
2025-08-24 18:00:56 -0400

    Time: 52 ms (8.67%) | Memory: 46.1 MB (39.59%) - LeetSync

--- 658-find-k-closest-elements/find-k-closest-elements.java
+++ 658-find-k-closest-elements/find-k-closest-elements.java
@@ -1,38 +1,24 @@
 class Solution {
     public List<Integer> findClosestElements(int[] arr, int k, int x) {
-     List<Integer> list = new ArrayList<>();
-        int index = arr.length-1;
-        // cambiar por busqueda binaria
+        List<Integer> list = new ArrayList<>();
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> {
+            int diffA = Math.abs(x - a);
+            int diffB = Math.abs(x - b);
+            if (diffA == diffB) {
+                return b - a;
+            }
+            return diffB - diffA;
+        });
         for (int i = 0; i < arr.length; i++) {
-            if (arr[i] >= x) {
-                index = i;
-                break;
+            pq.offer(arr[i]);
+            if (pq.size() > k) {
+                pq.poll();
             }
         }
-        int left = index - 1;
-        int right = index;
-        while (list.size() != k) {
-            if (left >= 0 && right < arr.length) {
-                int l = arr[left];
-                int r = arr[right];
-                int diff = Math.abs(x - l) - Math.abs(x - r);
-                if ((l == r) || (diff < 0) || (diff == 0 && l < r)) {
-                    list.add(l);
-                    left--;
-                } else {
-                    list.add(r);
-                    right++;
-                }
-            } else if (left < 0) {
-                list.add(arr[right]);
-                right++;
-            } else if (right >= arr.length) {
-                list.add(arr[left]);
-                left--;
-            }
-
+        for (int i = 0; i < k; i++) {
+            list.add(pq.poll());
         }
-        list.sort((a, b) -> a - b);
+        Collections.sort(list);
         return list;
     }
 }
