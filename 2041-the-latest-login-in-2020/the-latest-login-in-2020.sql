90a7493db84ab12cecc06c38464d25e3d086c832
gNOR-mu <82237339+gNOR-mu@users.noreply.github.com>
2025-08-23 22:04:34 -0400

    Time: 353 ms (85.37%) | Memory: 0B (100.00%) - LeetSync

---
+++ 2041-the-latest-login-in-2020/the-latest-login-in-2020.sql
@@ -0,0 +1,9 @@
+-- Write your PostgreSQL query statement below
+SELECT
+    USER_ID,
+    MAX(TIME_STAMP) AS LAST_STAMP
+FROM 
+    LOGINS
+
+WHERE EXTRACT(YEAR FROM TIME_STAMP) = 2020
+GROUP BY USER_ID
