4bb4be202e9f2ac5f9795fc368331bb458627c9b
gNOR-mu <82237339+gNOR-mu@users.noreply.github.com>
2025-08-24 00:07:51 -0400

    Time: 301 ms (76.49%) | Memory: 0B (100.00%) - LeetSync

---
+++ 577-employee-bonus/employee-bonus.sql
@@ -0,0 +1,10 @@
+-- Write your PostgreSQL query statement below
+SELECT
+    E.NAME,
+    B.BONUS
+FROM
+    EMPLOYEE AS E
+    LEFT JOIN BONUS AS B ON B.EMPID = E.EMPID
+WHERE
+    B.BONUS < 1000
+    OR B IS NULL;
