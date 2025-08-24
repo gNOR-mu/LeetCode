532bf9d38502ba52b930e0565728c96211ee39de
gNOR-mu <82237339+gNOR-mu@users.noreply.github.com>
2025-08-24 01:44:00 -0400

    Time: 4 ms (96.27%) | Memory: 44.7 MB (49.72%) - LeetSync

--- 657-robot-return-to-origin/robot-return-to-origin.java
+++ 657-robot-return-to-origin/robot-return-to-origin.java
@@ -2,12 +2,10 @@ class Solution {
     public boolean judgeCircle(String moves) {
         int[] mov = new int[2];
         for (char c:moves.toCharArray()) {
-            switch (c) {
-                case 'U' -> mov[0]++;
-                case 'D' -> mov[0]--;
-                case 'L' -> mov[1]--;
-                case 'R' -> mov[1]++;
-            }
+            if(c=='U') mov[0]++;
+            else if(c=='D') mov[0]--;
+            else if(c=='L')mov[1]--;
+            else if(c=='R')mov[1]++;
         }
         return mov[0] == 0 && mov[1]==0;
     }
