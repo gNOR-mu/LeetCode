006640adb1bc5c5f1897b221f000b55273096e0e
gNOR-mu <82237339+gNOR-mu@users.noreply.github.com>
2025-08-24 18:08:22 -0400

    Time: 33 ms (24.76%) | Memory: 46.8 MB (6.01%) - LeetSync

--- 658-find-k-closest-elements/find-k-closest-elements.java
+++ 658-find-k-closest-elements/find-k-closest-elements.java
@@ -1,38 +1,20 @@
 class Solution {
     public List<Integer> findClosestElements(int[] arr, int k, int x) {
-     List<Integer> list = new ArrayList<>();
-        int index = arr.length-1;
-        // cambiar por busqueda binaria
+        List<Integer> list = new ArrayList<>();
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
         for (int i = 0; i < arr.length; i++) {
-            if (arr[i] >= x) {
-                index = i;
-                break;
+            int val = arr[i];
+            if (k > 0) {
+                pq.offer(val);
+                k--;
+            } else if (Math.abs(pq.peek() - x) > Math.abs(val - x)) {
+                pq.poll();
+                pq.offer(val);
             }
         }
-        int left = index - 1;
-        int right = index;
-        while (list.size() != k) {
-            if (left >= 0 && right < arr.length) {
-                int l = arr[left];
-                int r = arr[right];
-                int diff = Math.abs(x - l) - Math.abs(x - r);
-                if ((l == r) || (diff < 0) || (diff == 0 && l < r)) {
-                    list.add(l);
-                    left--;
-                } else {
-                    list.add(r);
-                    right++;
-                }
-            } else if (left < 0) {
-                list.add(arr[right]);
-                right++;
-            } else if (right >= arr.length) {
-                list.add(arr[left]);
-                left--;
-            }
-
+        while (!pq.isEmpty()) {
+            list.add(pq.poll());
         }
-        list.sort((a, b) -> a - b);
         return list;
     }
 }
