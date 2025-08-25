01f740ad83c101f4233c0d11fa71d30bf47d3da5
gNOR-mu <82237339+gNOR-mu@users.noreply.github.com>
2025-08-25 04:05:38 -0400

    Time: 2 ms (2.13%) | Memory: 46 MB (6.21%) - LeetSync

---
+++ 745-find-smallest-letter-greater-than-target/find-smallest-letter-greater-than-target.java
@@ -0,0 +1,11 @@
+class Solution {
+    public char nextGreatestLetter(char[] letters, char target) {
+        PriorityQueue<Character> pq = new PriorityQueue<>();
+        for(char c:letters){
+            if(c>target){
+                pq.offer(c);
+            }
+        }
+        return pq.size()>0?pq.poll():letters[0];
+    }
+}
