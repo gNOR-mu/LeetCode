5d9c658a1f89f7fbb8fc24630bbde241681060c8
gNOR-mu <82237339+gNOR-mu@users.noreply.github.com>
2025-08-27 01:29:32 -0400

    Time: 18 ms (36.39%) | Memory: 46.4 MB (37.76%) - LeetSync

---
+++ 1427-all-elements-in-two-binary-search-trees/all-elements-in-two-binary-search-trees.java
@@ -0,0 +1,56 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> getAllElements(TreeNode root1, TreeNode root2) {
+        List<Integer> l1 = new ArrayList<>();
+        List<Integer> l2 = new ArrayList<>();
+        List<Integer> res = new ArrayList<>();
+
+        addToList(root1, l1);
+        addToList(root2, l2);
+
+        int l = 0;
+        int r = 0;
+        while (l < l1.size() || r < l2.size()) {
+            if (l == l1.size()) {
+                res.add(l2.get(r++));
+            } else if (r == l2.size()) {
+                res.add(l1.get(l++));
+            } else {
+                int v1 = l1.get(l);
+                int v2 = l2.get(r);
+                if (v1 <= v2) {
+                    l++;
+                    res.add(v1);
+                }else{
+                    r++;
+                    res.add(v2);
+                }
+            }
+
+        }
+        return res;
+    }
+
+    private void addToList(TreeNode root, List<Integer> list) {
+        if (root == null) {
+            return;
+        }
+        addToList(root.left, list);
+        list.add(root.val);
+        addToList(root.right, list);
+    }
+}
