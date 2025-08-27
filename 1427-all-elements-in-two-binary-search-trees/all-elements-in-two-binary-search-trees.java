beaa797b7fc59461284cf1f30893e647fef7b004
gNOR-mu <82237339+gNOR-mu@users.noreply.github.com>
2025-08-27 01:37:51 -0400

    Time: 35 ms (5.27%) | Memory: 46 MB (89.03%) - LeetSync

--- 1427-all-elements-in-two-binary-search-trees/all-elements-in-two-binary-search-trees.java
+++ 1427-all-elements-in-two-binary-search-trees/all-elements-in-two-binary-search-trees.java
@@ -15,43 +15,46 @@
  */
 class Solution {
     public List<Integer> getAllElements(TreeNode root1, TreeNode root2) {
-        List<Integer> l1 = new ArrayList<>();
-        List<Integer> l2 = new ArrayList<>();
+        // List<Integer> l1 = new ArrayList<>();
+        // List<Integer> l2 = new ArrayList<>();
         List<Integer> res = new ArrayList<>();
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        addToList(root1, pq);
+        addToList(root2, pq);
+        while(!pq.isEmpty()){
+            res.add(pq.poll());
+        }
 
-        addToList(root1, l1);
-        addToList(root2, l2);
-
-        int l = 0;
-        int r = 0;
-        while (l < l1.size() && r < l2.size()) {
-            int v1 = l1.get(l);
-            int v2 = l2.get(r);
-            if (v1 <= v2) {
-                l++;
-                res.add(v1);
-            } else {
-                r++;
-                res.add(v2);
-            }
-        }
-        while (l < l1.size()) {
-            res.add(l1.get(l));
-            l++;
-        }
-        while (r < l2.size()) {
-            res.add(l2.get(r));
-            r++;
-        }
+        // int l = 0;
+        // int r = 0;
+        // while (l < l1.size() && r < l2.size()) {
+        //     int v1 = l1.get(l);
+        //     int v2 = l2.get(r);
+        //     if (v1 <= v2) {
+        //         l++;
+        //         res.add(v1);
+        //     } else {
+        //         r++;
+        //         res.add(v2);
+        //     }
+        // }
+        // while (l < l1.size()) {
+        //     res.add(l1.get(l));
+        //     l++;
+        // }
+        // while (r < l2.size()) {
+        //     res.add(l2.get(r));
+        //     r++;
+        // }
         return res;
     }
 
-    private void addToList(TreeNode root, List<Integer> list) {
+    private void addToList(TreeNode root, PriorityQueue<Integer> pq) {
         if (root == null) {
             return;
         }
-        addToList(root.left, list);
-        list.add(root.val);
-        addToList(root.right, list);
+        addToList(root.left, pq);
+        pq.offer(root.val);
+        addToList(root.right, pq);
     }
 }
