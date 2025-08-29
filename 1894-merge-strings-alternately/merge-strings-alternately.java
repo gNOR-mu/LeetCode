8bf382e9773b43c9f73478e05b693ccd919dbffc
gNOR-mu <82237339+gNOR-mu@users.noreply.github.com>
2025-08-28 22:35:10 -0400

    Time: 1 ms (81.88%) | Memory: 42 MB (35.14%) - LeetSync

---
+++ 1894-merge-strings-alternately/merge-strings-alternately.java
@@ -0,0 +1,24 @@
+class Solution {
+    public String mergeAlternately(String word1, String word2) {
+        StringBuilder sb = new StringBuilder();
+        int w1 = 0;
+        int w2 = 0;
+
+        while (w1 < word1.length() && w2 < word2.length()) {
+            sb.append(word1.charAt(w1));
+            sb.append(word2.charAt(w2));
+            w1++;
+            w2++;
+        }
+        while (w1 < word1.length()) {
+            sb.append(word1.charAt(w1));
+            w1++;
+        }
+        while (w2 < word2.length()) {
+            sb.append(word2.charAt(w2));
+            w2++;
+        }
+        return sb.toString();
+
+    }
+}
