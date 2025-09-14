86e56c7574018e696b50b8a8e89f705fea4d414f
gNOR-mu <82237339+gNOR-mu@users.noreply.github.com>
2025-09-14 00:56:14 -0300

    Time: 4 ms (4.12%) | Memory: 47.2 MB (5.78%) - LeetSync

---
+++ 113-path-sum-ii/path-sum-ii.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
+        List<List<Integer>> res = new ArrayList<>();
+        sum(root, targetSum, new ArrayList<>(), res);
+        return res;
+    }
+
+    private void sum(TreeNode root, int targetSum, List<Integer> path, List<List<Integer>> res) {
+        if (root == null) {
+            return;
+        }
+        targetSum -= root.val;
+        path.add(root.val);
+        if(root.left == null && root.right==null && targetSum==0){
+            res.add(path);
+        }
+        sum(root.left, targetSum, new ArrayList(path), res);
+        sum(root.right, targetSum, new ArrayList(path), res);
+
+    }
+}
