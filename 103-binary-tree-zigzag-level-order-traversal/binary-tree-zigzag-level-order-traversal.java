6d72a3dd53553ea70f21915ea64402aa566f7a66
gNOR-mu <82237339+gNOR-mu@users.noreply.github.com>
2025-09-15 16:23:32 -0300

    Time: 0 ms (100.00%) | Memory: 42.2 MB (58.70%) - LeetSync

--- 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.java
+++ 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.java
@@ -18,33 +18,50 @@ class Solution {
         if (root == null) {
             return new ArrayList<>();
         }
-        Queue<TreeNode> queue = new LinkedList<>();
+        // Queue<TreeNode> queue = new LinkedList<>();
+        // List<List<Integer>> list = new ArrayList<>();
+
+        // queue.offer(root);
+        // boolean reversed = false;
+        // while (!queue.isEmpty()) {
+        //     int size = queue.size();
+        //     List<Integer> levelList = new ArrayList<>();
+
+        //     for (int i = 0; i < size; i++) {
+        //         TreeNode node = queue.poll();
+        //         //nivel par, lo agrego en orden normal
+        //         if (!reversed) {
+        //             levelList.add(node.val);
+        //         } else {
+        //             levelList.add(0, node.val);
+        //         }
+        //         if (node.left != null) {
+        //             queue.offer(node.left);
+        //         }
+        //         if (node.right != null) {
+        //             queue.offer(node.right);
+        //         }
+        //     }
+        //     reversed = !reversed;
+        //     list.add(levelList);
+        // }
+        // return list;
         List<List<Integer>> list = new ArrayList<>();
-
-        queue.offer(root);
-        boolean reversed = false;
-        while (!queue.isEmpty()) {
-            int size = queue.size();
-            List<Integer> levelList = new ArrayList<>();
-
-            for (int i = 0; i < size; i++) {
-                TreeNode node = queue.poll();
-                //nivel par, lo agrego en orden normal
-                if (!reversed) {
-                    levelList.add(node.val);
-                } else {
-                    levelList.add(0, node.val);
-                }
-                if (node.left != null) {
-                    queue.offer(node.left);
-                }
-                if (node.right != null) {
-                    queue.offer(node.right);
-                }
-            }
-            reversed = !reversed;
-            list.add(levelList);
-        }
+        helper(root, 0, list, false);
         return list;
+    }
+
+    private void helper(TreeNode root, int level, List<List<Integer>> list, boolean reverse) {
+        if (root == null) {
+            return;
+        }
+        if (list.size() == level) {
+            list.add(new ArrayList<>());
+        }
+        var listLevel = list.get(level);
+        int pos = reverse ? 0 : listLevel.size();
+        listLevel.add(pos, root.val);
+        helper(root.left, level + 1, list, !reverse);
+        helper(root.right, level + 1, list, !reverse);
     }
 }
