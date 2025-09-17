3f48650e9042566452cc4ee92a48bb002228cd90
gNOR-mu <82237339+gNOR-mu@users.noreply.github.com>
2025-09-16 21:20:28 -0300

    Time: 0 ms (100.00%) | Memory: 45.2 MB (69.07%) - LeetSync

--- 784-insert-into-a-binary-search-tree/insert-into-a-binary-search-tree.java
+++ 784-insert-into-a-binary-search-tree/insert-into-a-binary-search-tree.java
@@ -18,26 +18,12 @@ class Solution {
         if (root == null) {
             return new TreeNode(val);
         }
-        helper(root, val);
+        if (val < root.val) {
+            root.left = insertIntoBST(root.left, val);
+        } else if (val > root.val) {
+            root.right = insertIntoBST(root.right, val);
+        }
         return root;
     }
 
-    private void helper(TreeNode root, int val) {
-        if (root == null) {
-            return;
-        }
-        if (val < root.val) {
-            if (root.left == null) {
-                root.left = new TreeNode(val);
-            } else {
-                helper(root.left, val);
-            }
-        } else {
-            if (root.right == null) {
-                root.right = new TreeNode(val);
-            } else {
-                helper(root.right, val);
-            }
-        }
-    }
 }
