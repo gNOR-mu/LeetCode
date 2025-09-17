ecb39eec465f6f4154a64622312fce2c16cfb5f5
gNOR-mu <82237339+gNOR-mu@users.noreply.github.com>
2025-09-16 22:26:47 -0300

    Time: 18 ms (15.36%) | Memory: 83 MB (7.37%) - LeetSync

--- 1731-even-odd-tree/even-odd-tree.java
+++ 1731-even-odd-tree/even-odd-tree.java
@@ -15,36 +15,23 @@
  */
 class Solution {
     public boolean isEvenOddTree(TreeNode root) {
-        Queue<TreeNode> q = new LinkedList<>();
-        boolean isEvenLevel = true;
-        q.offer(root);
-        while (!q.isEmpty()) {
-            int size = q.size();
-            int min = Integer.MIN_VALUE;
-            int max = Integer.MAX_VALUE;
-            for (int i = 0; i < size; i++) {
-                TreeNode node = q.poll();
-                if (isEvenLevel) {
-                    if ((node.val & 1) == 0 || node.val <= min) {
-                        return false;
-                    }
-                    min = node.val;
-                } else if (!isEvenLevel) {
-                    if ((node.val & 1) == 1 || node.val >= max) {
-                        return false;
-                    }
-                    max = node.val;
-                }
-                if (node.left != null) {
-                    q.offer(node.left);
-                }
-                if (node.right != null) {
-                    q.offer(node.right);
-                }
-            }
+        Map<Integer, Integer> map = new HashMap<>();
+        return helper(root, 0, map);
+    }
 
-            isEvenLevel = !isEvenLevel;
+    private boolean helper(TreeNode root, int level, Map<Integer, Integer> map) {
+        if (root == null) {
+            return true;
         }
-        return true;
+        Integer previous = map.get(level);
+        boolean isEven = (root.val & 1) == 0;
+        boolean isEvenLevel = (level & 1) == 0;
+        if (isEvenLevel && (isEven || (previous != null && root.val <= previous))) {
+            return false;
+        } else if (!isEvenLevel && (!isEven || (previous != null && root.val >= previous))) {
+            return false;
+        }
+        map.put(level, root.val);
+        return helper(root.left, level + 1, map) && helper(root.right, level + 1, map);
     }
 }
