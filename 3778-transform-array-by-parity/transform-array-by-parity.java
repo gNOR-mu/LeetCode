e94857e4cdbdb360041e9c584a5e5a7f97e2b5c8
gNOR-mu <82237339+gNOR-mu@users.noreply.github.com>
2025-09-21 15:54:24 -0300

    Time: 1 ms (100.00%) | Memory: 45.6 MB (6.29%) - LeetSync

--- 3778-transform-array-by-parity/transform-array-by-parity.java
+++ 3778-transform-array-by-parity/transform-array-by-parity.java
@@ -1,14 +1,11 @@
 class Solution {
     public int[] transformArray(int[] nums) {
         int[] res = new int[nums.length];
-        int odds = 0;
+        int idx = nums.length-1;
         for (int i = 0; i < nums.length; i++) {
             if ((nums[i] & 1) == 1) {
-                odds += 1;
+                res[idx--] = 1;
             }
-        }
-        for (int i = nums.length - odds; i < nums.length; i++) {
-            res[i] = 1;
         }
         return res;
     }
