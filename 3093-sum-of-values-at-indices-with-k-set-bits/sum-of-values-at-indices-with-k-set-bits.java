7d97e60bc08cfd0a7bda7e831515ccc590260fae
gNOR-mu <82237339+gNOR-mu@users.noreply.github.com>
2026-02-08 22:16:51 -0300

    Time: 1 ms (100.00%) | Memory: 46.5 MB (80.49%) - LeetSync

--- 3093-sum-of-values-at-indices-with-k-set-bits/sum-of-values-at-indices-with-k-set-bits.java
+++ 3093-sum-of-values-at-indices-with-k-set-bits/sum-of-values-at-indices-with-k-set-bits.java
@@ -2,15 +2,7 @@ class Solution {
     public int sumIndicesWithKSetBits(List<Integer> nums, int k) {
         int sum = 0;
         for (int i = 0; i < nums.size(); i++) {
-            String bs = Integer.toBinaryString(i);
-            int kBits = 0;
-            for (char c : bs.toCharArray()) {
-                if (c == '1') {
-                    kBits++;
-                }
-            }
-
-            if (kBits == k) {
+            if(Integer.bitCount(i) == k){
                 sum += nums.get(i);
             }
         }
